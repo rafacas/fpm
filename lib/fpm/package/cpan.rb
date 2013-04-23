19181817855be7164c6713542c25124a152376db
Rafael Casado Sanchez <rafacas@gmail.com>
2013-04-23 15:09:46 +0100

    Started working on the CPAN feature

---
+++ lib/fpm/package/cpan.rb
@@ -0,0 +1,13 @@
+require "fpm/namespace"
+require "fpm/package"
+require "fpm/util"
+
+class FPM::Package::CPAN < FPM::Package
+
+  private
+  def input(package)
+
+  end
+
+  public(:input)
+end # class FPM::Package::CPAN
